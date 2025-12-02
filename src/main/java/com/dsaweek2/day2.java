5123d97a0bd0f85f8bddb90b9d681ab27cc75ed2
Karan Rathod <kr04391@gmail.com>
2025-12-02 22:16:34 +0530

    LC 34 : DONE

--- src/main/java/com/dsaweek2/day2.java
+++ src/main/java/com/dsaweek2/day2.java
@@ -2,11 +2,16 @@ package com.dsaweek2;
 
 public class day2 {
     public static void main(String[] args) {
-        // LC  : find minimum in rotated array
-        int[] nums = {3, 4, 5, 6, 0, 1, 2};
-        int result = findMinimum(nums);
-        System.out.println(result);
+
+        int[] nums = {5,7,7,8,8,10};
+        int n =  nums.length;
+        int target = 8;
+
+//        int result = findMinimum(nums);
+
+        System.out.println(searchPosition(nums, target));
     }
+    // LC 153 : find minimum in rotated array
     public static  int findMinimum(int[] nums){
         int n = nums.length;
         int l = 0, r = n - 1;
@@ -21,4 +26,45 @@ public class day2 {
         return nums[r];
     }
 
+
+    // LC 34 : FIND FIRST AND LAST POSITION OF ELEMENT IN SORTED ARRAY
+    public static int[] searchPosition(int[] nums, int target){
+        int n = nums.length;
+        int leftPosition = leftMostPosition(nums, target, n);
+        int rightPosition = rightMostPositon(nums, target, n);
+
+        return new int[] {leftPosition, rightPosition};
+    }
+    public static int leftMostPosition(int[] nums, int target, int n){
+        int leftMost = -1;
+        int l = 0, r = n- 1;
+        while(l <= r){
+            int mid = l +(r - l)/2;
+            if(nums[mid] == target){
+                leftMost = mid;
+                r = mid - 1;
+            }else if(nums[mid] > target){
+                r = mid - 1;
+            }else {
+                l = mid + 1;
+            }
+        }
+        return leftMost;
+    }
+    public static int rightMostPositon(int[] nums, int target, int n ){
+        int rightMost = -1;
+        int l = 0, r = n - 1;
+        while( l <= r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] == target){
+                rightMost = mid;
+                l = mid + 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            }else {
+                r = mid - 1;
+            }
+        }
+        return rightMost;
+    }
 }
