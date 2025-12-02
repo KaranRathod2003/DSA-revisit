183be9acdd7ecef662cf9b9f36444305005a5fe6
Karan Rathod <kr04391@gmail.com>
2025-12-02 22:26:27 +0530

    LC 33 : DONE

--- src/main/java/com/dsaweek2/day2.java
+++ src/main/java/com/dsaweek2/day2.java
@@ -3,13 +3,13 @@ package com.dsaweek2;
 public class day2 {
     public static void main(String[] args) {
 
-        int[] nums = {5,7,7,8,8,10};
+        int[] nums = {4,5,6,7,0,1,2};
         int n =  nums.length;
-        int target = 8;
+        int target = 4;
 
 //        int result = findMinimum(nums);
-
-        System.out.println(searchPosition(nums, target));
+            int result = searchArray(nums, target);
+        System.out.println(result);
     }
     // LC 153 : find minimum in rotated array
     public static  int findMinimum(int[] nums){
@@ -66,5 +66,44 @@ public class day2 {
             }
         }
         return rightMost;
+    }
+
+    // LC 33 : SEARCH IN ROTATED ARRAY
+    public static int searchArray(int[] nums, int target){
+        int n = nums.length;
+        int pivotElem = findPivot(nums, n);
+        int idx = binarySearch(0, pivotElem, nums, target);
+        if(idx != -1){
+            return idx;
+        }
+        idx = binarySearch(pivotElem, n -1, nums, target);
+        return idx;
+    }
+    public static int findPivot(int[] nums, int n){
+        int l = 0, r = n - 1;
+        while(l < r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] > nums[r]){
+                l = mid + 1;
+            }else{
+                r = mid;
+            }
+        }
+        return r;
+    }
+    public static int binarySearch(int l, int r, int[] nums, int target){
+        int idx = -1;
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] == target){
+                idx = mid;
+                break;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            }else {
+                l = mid + 1;
+            }
+        }
+        return idx;
     }
 }
